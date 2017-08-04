ce4fa6d9a1092d46a59a70a452c2daaa9c303da2
nnddkj <1647294495@qq.com>
2017-08-05 02:40:39 +0800

    添加一个变量测试程序版本恢复过程

--- app/src/main/java/com/nnddkj/wisdommask/testdemo/MainActivity.java
+++ app/src/main/java/com/nnddkj/wisdommask/testdemo/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String data="测试程序";
         setContentView(R.layout.activity_main);
     }
 }
